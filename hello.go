9d2efeaba3ef5c6ba787a1d8e324126da0fa2fe9
markokeeffe <m.okeeffe@veneficus.co.uk>
2016-05-12 07:59:03 +1000

    refactored

--- hello.go
+++ hello.go
@@ -1,27 +1,25 @@
-package main
+package goproxy
 
 import "fmt"
 import "net/http"
 import "encoding/json"
 
-func main() {
+const TaskTypeDbMysqlQuery = 1
+const TaskTypeDbMysqlExec = 2
 
-	const TaskTypeDbMysqlQuery = 1
-	const TaskTypeDbMysqlExec = 2
+type MysqlTaskConfig struct {
+	Dsn string                `json:"dsn"`
+}
+type Task struct {
+	RawConfig json.RawMessage        `json:"config"`
+	Type      uint64                `json:"type"`
+	Payload   string                `json:"payload"`
 
-	type MysqlTaskConfig struct{
-		Dsn		string		`json:"dsn"`
-	}
-	type Task struct{
-		RawConfig	json.RawMessage	`json:"config"`
-		Type		uint64		`json:"type"`
-		Payload		string		`json:"payload"`
+	Config    MysqlTaskConfig
+}
 
-		Config		MysqlTaskConfig
-	}
-
-
-	resp, err := http.Get("http://taskserver:8888/")
+func getTask (url string) Task {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
@@ -42,6 +40,12 @@ func main() {
 		task.Config = config;
 	}
 
+	return task
+}
+
+func main() {
+
+	var task = getTask("http://taskserver:8888/")
 
 	fmt.Println(task.Config)
 }
